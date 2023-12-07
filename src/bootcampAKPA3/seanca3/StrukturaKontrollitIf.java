49106039f5cf3c5784de3f00e7fe5a4b89e74585
Kamberi Anjeza <anjeza.kamberi@eng.it>
2023-12-07 20:32:36 +0100

    perditesuar main struktura kontrolli if - test git

--- src/bootcampAKPA3/seanca3/StrukturaKontrollitIf.java
+++ src/bootcampAKPA3/seanca3/StrukturaKontrollitIf.java
@@ -4,7 +4,7 @@ public class StrukturaKontrollitIf {
 
 	public static void main(String[] args) {
 		System.out.println("Struktura e kontrollit IF");
-
+		System.out.println("Fund struktura kontrolli IF");
 	}
 
 }
